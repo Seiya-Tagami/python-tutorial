0aec3c36dfa17c675e7f7f7292a3e6bec94765d1
Seiya-Tagami <bepozurapu274@gmail.com>
2023-03-09 14:57:32 +0900

    done

--- python-basic-tutorial_yt/ls2.py
+++ python-basic-tutorial_yt/ls2.py
@@ -11,6 +11,27 @@ else:
 print(x >= 10 or y >= 9)
 print(x >= 10 and not y >= 10)
 
+
+# 三項演算子
+age = 18
+result = '成人' if age >= 20 else '子供' if age >= 1 else '赤ちゃん'
+print(result)
+
+
+# セイウチ演算子・代入式
+x = list(range(3))
+if(n:=len(x)) >= 10:
+    print('listが長すぎます')
+
+print(f'n:{n}')
+
+import re
+
+s='合計金額は1200円です'
+if(m:=re.search(r'[0-9]+円', s)):
+    print(m.group())
+
+
 # for文
 for t in range(10):
     print('Pythonプログラミング Vtuber サプー')
